1a81e9f7e540f2ace6e2444362cc397ef41029bf
Devyn Cyphers <devcon4444@gmail.com>
2015-09-06 01:00:15 -0600

    update-1
    Author: Devyn Cyphers; Devcon

--- Assets/Code/Managers/Master.cs
+++ Assets/Code/Managers/Master.cs
@@ -2,7 +2,7 @@
 
 public class Master : MonoBehaviour {
     public static Master master;
-
+    public GameObject[] cameras;
     void Awake() {
         if (master == null) {
             DontDestroyOnLoad(gameObject);
